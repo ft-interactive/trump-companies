7b9283353049e66b22248889ea91835b95a5f549
Joanna Kao <joanna.kao@ft.com>
2017-02-06 14:55:19 -0500

    set pub date

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2017-02-03T15:23:00Z'),
 
   headline: 'Companies react to Donald Trump',
 
